780a5035923d56218151516ad4e299cdbce04660
GabrieleMoreale <GabrieleMoreale@users.noreply.github.com>
2021-10-29 14:25:12 +0200

    Rename

--- six/src/main/java/it/euris/academy/six/repository/ProjectionRepository.java
+++
@@ -1,8 +0,0 @@
-package it.euris.academy.six.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import it.euris.academy.six.data.model.Projection;
-
-public interface ProjectionRepository extends JpaRepository<Projection, Long> {
-
-}
---
+++ six/src/test/java/it/euris/academy/cinema/CinemaTests.java
@@ -0,0 +1,13 @@
+package it.euris.academy.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
